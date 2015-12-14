805a4249cb201a52c29f88d9876358774a0f48e6
Jes Frellsen <jf519@cam.ac.uk>
2015-12-14 14:47:29 +0000

    Corrected formatting issues in plot.py

--- scripts/plot.py
+++ scripts/plot.py
@@ -23,18 +23,18 @@
 
 import numpy
 from numpy import array, log, zeros
-from re import findall
 from muninn.plot_histogram import plot_histogram
 from muninn.utils import pickle_to_file
+
 
 def plot_name_array(pdf, name, data, ndims, counts=False, bins=None, support=None, fig=None,
                     xlab=None, ylab="", xmin=None, xmax=None, main=None,
                     bin_numbers=True, color="blue"):
 
     # Set the title
-    if main==None:
+    if main is None:
         this_main = name
-    elif main=="":
+    elif main == "":
         this_main = None
     else:
         this_main = main
@@ -47,25 +47,26 @@ def plot_name_array(pdf, name, data, ndims, counts=False, bins=None, support=Non
 
     # Check that the data is floats (othervise R might give problems)
     if data.dtype == numpy.int32:
-       data = array(data, dtype=float)
-    
+        data = array(data, dtype=float)
+
     # Do the plotting
-    if ndims==1:
+    if ndims == 1:
         return plot_histogram(pdf, data, color=color, main=this_main, bins=bins, support=support, fig=fig,
-                      xlab=xlab, ylab=ylab, sub=sub, xmin=xmin, xmax=xmax, bin_numbers=bin_numbers)
+                              xlab=xlab, ylab=ylab, sub=sub, xmin=xmin, xmax=xmax, bin_numbers=bin_numbers)
     else:
         print "Cannot plot %d dimensions!" % ndims
+
 
 def plot_binning(pdf, entries, main=None, color="blue"):
     for entry in entries:
         number, fullname, tarray = entry
 
-        if main==None:
+        if main is None:
             this_main = fullname
         else:
             this_main = main
 
-        if len(tarray)>0:
+        if len(tarray) > 0:
             fig = plt.figure()
             ax = fig.add_subplot(111)
             ax.plot(range(len(tarray)), tarray, color=color)
@@ -73,6 +74,7 @@ def plot_binning(pdf, entries, main=None, color="blue"):
             ax.set_ylabel("center value")
             ax.set_title(this_main)
             pdf.savefig()
+
 
 def plot_bin_widths(pdf, entries, log_space=False, main=None, color="blue"):
     for entry in entries:
@@ -84,12 +86,12 @@ def plot_bin_widths(pdf, entries, log_space=False, main=None, color="blue"):
         else:
             ylab = r"$\Delta_\mathrm{bin})$"
 
-        if main==None:
+        if main is None:
             this_main = fullname
         else:
             this_main = main
-        
-        if len(tarray)>0:
+
+        if len(tarray) > 0:
             fig = plt.figure()
             ax = fig.add_subplot(111)
             ax.plot(range(len(tarray)), tarray, color=color)
@@ -97,6 +99,7 @@ def plot_bin_widths(pdf, entries, log_space=False, main=None, color="blue"):
             ax.set_ylabel(ylab)
             ax.set_title(this_main)
             pdf.savefig()
+
 
 def plot_entry_list(pdf, log_entry_list, binning=None, binning_dict=None, bin_widths=None, bin_widths_dict=None,
                     support_dict=None, xlab=None, ylab="", normalize_log_space=True, xmin=None, xmax=None,
@@ -110,22 +113,22 @@ def plot_entry_list(pdf, log_entry_list, binning=None, binning_dict=None, bin_wi
         this_ylab = ylab
 
         # Decide on which binning to use
-        if binning_dict!=None and binning_dict.has_key(number):
+        if binning_dict is not None and number in binning_dict:
             bins = binning_dict[number][2]
-        elif binning!=None and len(binning)==1 and len(binning[0][2])-1==len(tarray):
+        elif binning is not None and len(binning) == 1 and len(binning[0][2])-1 == len(tarray):
             bins = binning[0][2]
         else:
             bins = None
 
-        # Normalize by bin width, if the binwidh is available 
-        if bin_widths_dict!=None and bin_widths_dict.has_key(number):
+        # Normalize by bin width, if the binwidh is available
+        if bin_widths_dict is not None and number in bin_widths_dict:
             widths = bin_widths_dict[number][2]
-        elif bin_widths!=None and len(bin_widths)==1 and len(bin_widths[0][2])==len(tarray):
+        elif bin_widths is not None and len(bin_widths) == 1 and len(bin_widths[0][2]) == len(tarray):
             widths = bin_widths[0][2]
         else:
             widths = None
 
-        if widths!=None:
+        if widths is not None:
             if normalize_log_space:
                 tarray -= log(widths)
                 this_ylab = r"$%s - \ln(\Delta_\mathrm{bin})$" % ylab.replace("$", "")
@@ -134,7 +137,7 @@ def plot_entry_list(pdf, log_entry_list, binning=None, binning_dict=None, bin_wi
                 this_ylab = r"$%s / \Delta_\mathrm{bin}$" % ylab.replace("$", "")
 
         # Decide on which support to use
-        if support_dict!=None and support_dict.has_key(number):
+        if support_dict is not None and number in support_dict:
             support = support_dict[number][2]
         else:
             support = None
@@ -148,22 +151,27 @@ def plot_entry_list(pdf, log_entry_list, binning=None, binning_dict=None, bin_wi
 
     return points
 
+
+def l2norm(v):
+    return numpy.sqrt(numpy.dot(v, v))
+
+
 def plot_sum_N(pdf, log_entry_list, binning=None, binning_dict=None, bin_widths=None, bin_widths_dict=None,
-                    support_dict=None, xlab=None, ylab="", normalize_log_space=True, xmin=None, xmax=None, main=None, bin_numbers=True, color="blue"):
+               support_dict=None, xlab=None, ylab="", normalize_log_space=True, xmin=None, xmax=None, main=None, bin_numbers=True, color="blue"):
     entries = convert_log_entries(log_entry_list)
 
-    if binning!=None and binning_dict!=None and len(binning_dict)>0:
+    if binning is not None and binning_dict is not None and len(binning_dict) > 0:
         newest = sorted(binning_dict.keys())[-1]
 
         # Added up the entries in the entry list
         bins = binning_dict[newest][2]
         sums = zeros(bins.shape[0]-1, dtype=entries[0][2].dtype)
-        
+
         for entry in entries:
             number, fullname, tarray = entry
 
             # See if we need to align the binning
-            if binning_dict.has_key(number):
+            if number in binning_dict:
                 this_bins = binning_dict[number][2]
 
                 size_diff = bins.shape[0] - this_bins.shape[0]
@@ -174,20 +182,18 @@ def plot_sum_N(pdf, log_entry_list, binning=None, binning_dict=None, bin_widths=
                     return
 
                 # Find the best alignment
-                norm = lambda v: numpy.sqrt(numpy.dot(v,v))
                 minimal = None
 
-
                 for i in range(size_diff+1):
-                    alignment_diff = norm(bins[i:bins.shape[0]-(size_diff-i)] - this_bins)
+                    alignment_diff = l2norm(bins[i:bins.shape[0]-(size_diff-i)] - this_bins)
 
-                    if minimal==None or alignment_diff < minimal[0]:
+                    if minimal is None or alignment_diff < minimal[0]:
                         minimal = (alignment_diff, i)
 
-                if minimal[0]>0.1:
+                if minimal[0] > 0.1:
                     print "Error aligning binning arrays: No good alignment found."
                     return
-                    
+
                 # Check that the count array has the right shape
                 if (this_bins.shape[0]-1) != tarray.shape[0]:
                     print "Mismatch in size for binning and count array for iteration %d" % number
@@ -205,18 +211,18 @@ def plot_sum_N(pdf, log_entry_list, binning=None, binning_dict=None, bin_widths=
 
                 sums += tarray
 
-        # Normalize by bin width, if the binwidh is available 
-        if bin_widths_dict!=None and bin_widths_dict.has_key(newest):
+        # Normalize by bin width, if the binwidh is available
+        if bin_widths_dict is not None and newest in bin_widths_dict:
             widths = bin_widths_dict[newest][2]
         else:
             widths = None
 
-        if widths!=None:
+        if widths is not None:
             if normalize_log_space:
                 sums -= log(widths)
                 this_ylab = r"$%s - \ln(\Delta_\mathrm{bin})$" % ylab.replace("$", "")
             else:
-                sums = sums/widths  # Note, /= worn't work, since tarray might be an int array               
+                sums = sums/widths  # Note, /= worn't work, since tarray might be an int array
                 this_ylab = r"$%s / \Delta_\mathrm{bin}$" % ylab.replace("$", "")
 
             counts = False
@@ -225,7 +231,6 @@ def plot_sum_N(pdf, log_entry_list, binning=None, binning_dict=None, bin_widths=
             this_ylab = ylab
             counts = True
             name = "sum_N"
-
 
         # Do the plotting
         p = plot_name_array(pdf, "Accumulated", sums, 1, counts=counts, bins=bins, support=None, fig=None,
@@ -257,45 +262,45 @@ if __name__ == "__main__":
     parser.add_argument("-b", dest="bins", default=False, action="store_true", help="Plot the binning")
 
     parser.add_argument("--start", dest="start", metavar="VAL", type=int, default=None,
-                      help="The first array number to plot, a negative value means " +
-                           "counting from the end (default: %(default)s)")
+                        help="The first array number to plot, a negative value means " +
+                             "counting from the end (default: %(default)s)")
     parser.add_argument("--end", dest="end", metavar="VAL", type=int, default=None,
-                      help="The last but one array number to plot, negative values " +
-                           "are not allowed (default: %(default)s).")
+                        help="The last but one array number to plot, negative values " +
+                             "are not allowed (default: %(default)s).")
     parser.add_argument("-i", dest="indices", metavar="VAL", default=[], action="append",
-                      help="Index for the array number to plot - options can be used multiple times.")
+                        help="Index for the array number to plot - options can be used multiple times.")
     parser.add_argument("--xmin", dest="xmin", metavar="FLOAT", type=float, default=None,
-                      help="The minimal x-value to plot.")
+                        help="The minimal x-value to plot.")
     parser.add_argument("--xmax", dest="xmax", metavar="FLOAT", type=float, default=None,
-                      help="The maximal x-value to plot.")
+                        help="The maximal x-value to plot.")
     parser.add_argument("--width", dest="width", metavar="FLOAT", type=float, default=10.,
-                      help="The width of the graphics region in inches (default: %(default)s).")
+                        help="The width of the graphics region in inches (default: %(default)s).")
     parser.add_argument("--height", dest="height", metavar="FLOAT", type=float, default=7.,
-                      help="The height of the graphics region in inches (default: %(default)s).")
+                        help="The height of the graphics region in inches (default: %(default)s).")
     parser.add_argument("--fontsize", dest="fontsize", metavar="INT", type=int, default=14,
-                      help="The font size (default: %(default)s).")
+                        help="The font size (default: %(default)s).")
     parser.add_argument("--color", dest="color", metavar="COLOR", type=str, default="blue",
-                      help="The plot color (default: %(default)s).")
+                        help="The plot color (default: %(default)s).")
     parser.add_argument("--main", dest="main", metavar="TEXT", type=str, default=None,
-                      help="Set a different title than the default.")
+                        help="Set a different title than the default.")
     parser.add_argument("--xlab", dest="xlab", metavar="TEXT", type=str, default=None,
-                      help="Set a different label on the x-axis than 'E'.")
+                        help="Set a different label on the x-axis than 'E'.")
     parser.add_argument("--no-bins", dest="bin_numbers", action="store_false", default=True,
-                      help="Disable bin numbers on the x-axis.")
+                        help="Disable bin numbers on the x-axis.")
     args = parser.parse_args()
 
     # Check the arguments
     if not os.access(args.muninn_log_file, os.R_OK):
         parser.error("File '" + args.muninn_log_file + "' not accessible.")
 
-    if (args.start!=None or args.end!=None) and args.indices!=[]:
+    if (args.start is not None or args.end is not None) and args.indices != []:
         parser.error("Options --start and --end are mutually exclusive with -i.")
 
-    if args.end!=None:
-        if args.end<0:
+    if args.end is not None:
+        if args.end < 0:
             parser.error("argument --end: negative end value not allowed.")
 
-    if args.indices!=[]:
+    if args.indices != []:
         try:
             args.indices = map(int, args.indices)
         except ValueError:
@@ -311,7 +316,6 @@ if __name__ == "__main__":
     bin_widths = convert_log_entries(log_dict.get('bin_widths', []))
     bin_widths_dict = dict(map(lambda entry: (entry[0], entry), bin_widths))
 
-
     # Plot the required output
     pdf = PdfPages(args.output)
     matplotlib.rc('font', size=args.fontsize)
@@ -321,7 +325,7 @@ if __name__ == "__main__":
     xmax = args.xmax
 
     points = []
-        
+
     if args.lng:
         support = convert_log_entries(log_dict.get('lnG_support', []))
         support_dict = dict(map(lambda entry: (entry[0], entry), support))
@@ -332,35 +336,34 @@ if __name__ == "__main__":
 
     if args.lnw:
         p = plot_entry_list(pdf, log_dict.get('lnw', []), binning, binning_dict, None, None,
-                               xlab=args.xlab, ylab=r"$\ln(w)$", xmin=xmin, xmax=xmax, main=args.main,
-                               bin_numbers=args.bin_numbers, color=args.color)
+                            xlab=args.xlab, ylab=r"$\ln(w)$", xmin=xmin, xmax=xmax, main=args.main,
+                            bin_numbers=args.bin_numbers, color=args.color)
         points += p
 
     if args.n:
-        p = plot_entry_list(pdf, log_dict.get('N',[]), binning, binning_dict, bin_widths, bin_widths_dict,
+        p = plot_entry_list(pdf, log_dict.get('N', []), binning, binning_dict, bin_widths, bin_widths_dict,
                             xlab=args.xlab, ylab=r"$N$", normalize_log_space=False, xmin=xmin, xmax=xmax,
                             main=args.main, bin_numbers=args.bin_numbers, color=args.color)
         points += p
 
     if args.N:
-        p = plot_entry_list(pdf, log_dict.get('N',[]), binning, binning_dict, None, None,
+        p = plot_entry_list(pdf, log_dict.get('N', []), binning, binning_dict, None, None,
                             xlab=args.xlab, ylab=r"$N$", normalize_log_space=False, xmin=xmin, xmax=xmax,
                             main=args.main, bin_numbers=args.bin_numbers, color=args.color)
         points += p
 
-
     if args.s:
-        p = plot_sum_N(pdf, log_dict.get('N',[]), binning, binning_dict, bin_widths, bin_widths_dict,
+        p = plot_sum_N(pdf, log_dict.get('N', []), binning, binning_dict, bin_widths, bin_widths_dict,
                        xlab=args.xlab, ylab=r"$N$", normalize_log_space=False, xmin=xmin, xmax=xmax,
                        main=args.main, bin_numbers=args.bin_numbers, color=args.color)
         points += p
 
     if args.S:
-        p = plot_sum_N(pdf, log_dict.get('N',[]), binning, binning_dict, None, None,
+        p = plot_sum_N(pdf, log_dict.get('N', []), binning, binning_dict, None, None,
                        xlab=args.xlab, ylab=r"$N$", normalize_log_space=False, xmin=xmin, xmax=xmax,
                        main=args.main, bin_numbers=args.bin_numbers, color=args.color)
         points += p
-        
+
     if args.bins:
         plot_binning(pdf, binning, main=args.main, color=args.color)
         plot_bin_widths(pdf, bin_widths, log_space=False, main=args.main, color=args.color)
@@ -369,5 +372,5 @@ if __name__ == "__main__":
     pdf.close()
 
     # Dump the output as a pickle
-    if args.pickle!=None:
+    if args.pickle is not None:
         pickle_to_file(points, args.pickle)
